a1287d3f0ad8302510c36bf5a67605b47f265547
kajeme11 <chenequek@gmail.com>
2025-08-07 21:19:30 -0700

    Restricted admin controller access to admin only using PreAuthorize annotation

--- src/main/java/com/login/loginSecure/controller/AdminController.java
+++ src/main/java/com/login/loginSecure/controller/AdminController.java
@@ -6,22 +6,26 @@ import com.login.loginSecure.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     @Autowired
     private UserService userService;
 
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getUsers")
     public ResponseEntity<List<User>> getUsers(){
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/update-role")
     public ResponseEntity<String> updateUser(@RequestParam Long id,
                                              @RequestParam String roleName){
@@ -29,6 +33,7 @@ public class AdminController {
         return ResponseEntity.ok("User Role Updated!");
     }
 
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id){
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
